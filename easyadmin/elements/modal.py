dba105b5a4d4466b58644e1ee39e16164d1f8bf7
codemation <joshjamison1@gmail.com>
2021-04-11 13:53:58 +0200

    added size paramter

--- easyadmin/elements/modal.py
+++ easyadmin/elements/modal.py
@@ -2,13 +2,15 @@ def get_modal(
     name: str,
     alert: str = "DEFAULT ALERT STRING",
     body: str = "Extra details pertaining to Modal, or html",
-    footer: str = "DEFAULT MODAL FOOTER HTML / TEXT"
+    footer: str = "DEFAULT MODAL FOOTER HTML / TEXT",
+    size: str = None,
 ):
+    size = f'-{size}' if size else ''
     return f"""
 <!-- {name} Modal-->
 <div class="modal fade" id="{name}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
     aria-hidden="true">
-    <div class="modal-dialog" role="document">
+    <div class="modal-dialog modal{size}" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">{alert}</h5>
