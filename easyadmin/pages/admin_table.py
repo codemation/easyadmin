1b8bee8d6f04621645d375cf3c353350d175732c
codemation <joshjamison1@gmail.com>
2021-04-13 08:57:02 +0200

    admin_table derives from admin_page

--- easyadmin/pages/admin_table.py
+++ easyadmin/pages/admin_table.py
@@ -1,3 +1,46 @@
+from .admin import get_admin_page
+
+def get_table_body(
+    name: str,
+    table_headers_html: str,
+    table_rows_html: str,
+    above: str = '',
+    below: str = ''
+):
+    return f"""
+<!-- Page Heading -->
+{above}
+
+<!-- DataTales Example -->
+<div class="card shadow mb-4">
+    <div class="card-header py-3">
+        <h6 class="m-0 font-weight-bold text-primary">{name}</h6>
+    </div>
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                <thead>
+                    <tr>
+                        {table_headers_html}
+                    </tr>
+                </thead>
+                <tfoot>
+                    <tr>
+                        {table_headers_html}
+                    </tr>
+                </tfoot>
+                <tbody>
+                    {table_rows_html}
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+{below}
+"""
+
+
 def get_table_page(
     name: str, 
     data: list,
@@ -23,6 +66,20 @@ def get_table_page(
 </tr>
 """)
     table_rows_html = ''.join(table_rows_html)
+    admin_page = get_admin_page(
+        name=name,
+        sidebar=sidebar,
+        body=get_table_body(
+            name,
+            table_headers_html,
+            table_rows_html,
+            above=above,
+            below=below
+        ),
+        current_user=current_user,
+        modals=modals
+    )
+    return admin_page
 
     return f"""
 <!DOCTYPE html>
