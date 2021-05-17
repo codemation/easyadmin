ee03e1d902b5416576e2de817acbc23a3cd2ee97
codemation <joshjamison1@gmail.com>
2021-05-17 11:26:48 +0200

    corrected issues with split-button data-toggle & onclick used togather

--- easyadmin/elements/buttons.py
+++ easyadmin/elements/buttons.py
@@ -9,22 +9,33 @@ def get_split_button(
 ):
     size = f' btn-{size}' if size else ''
     href = f' href="{href}"' if href else ''
-
     if onclick:
-        onclick = f' onclick="{onclick}"'
+        html_onclick = f' onclick="{onclick}"'
+    else:
+        html_onclick = ''
+
     if modal:
         modal = f' data-toggle="modal" data-target="#{modal}"'
     else:
         modal = ""
 
-    return f"""
-<a{href} class="btn btn-{color} btn-icon-split{size}"{modal}>
+    button = f"""
+<a{href} class="btn btn-{color} btn-icon-split{size}"{modal}{html_onclick}>
     <span class="icon text-white-50">
         <i class="fas fa-{icon}"></i>
     </span>
     <span class="text">{name}</span>
 </a>
 """
+    if onclick and modal:
+        button = f"""{button}
+<script>
+$(document).on("click", "#{name}", function() {{
+    {onclick}()
+}});
+</script>
+"""
+    return button
 def get_button(
     name: str = "DEFAULT_BUTTON_NAME",
     href: str = None,
