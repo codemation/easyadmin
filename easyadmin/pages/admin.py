d5db5a1398d8326015bea134ede174a2c27d62f9
codemation <joshjamison1@gmail.com>
2021-05-24 20:01:19 +0200

    removed references to static content that exists externally, preventing 404's on load

--- easyadmin/pages/admin.py
+++ easyadmin/pages/admin.py
@@ -146,28 +146,6 @@ def get_admin_page(
 
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
-    ###
-
-    <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-
-    <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
-
-    <!-- Page level plugins -->
-    <script src="vendor/datatables/jquery.dataTables.min.js"></script>
-    <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
-
-    <!-- Page level custom scripts -->
-    <script src="js/demo/datatables-demo.js"></script>
-
-
-
-
 </body>
 
 </html>
