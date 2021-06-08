78bea27aa8af20784e6626c16a2c91a74f317b11
codemation <joshjamison1@gmail.com>
2021-06-08 16:27:42 +0200

    removed footer from admin page

--- easyadmin/pages/admin.py
+++ easyadmin/pages/admin.py
@@ -105,16 +105,6 @@ def get_admin_page(
             </div>
             <!-- End of Main Content -->
 
-            <!-- Footer -->
-            <footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2020</span>
-                    </div>
-                </div>
-            </footer>
-            <!-- End of Footer -->
-
         </div>
         <!-- End of Content Wrapper -->
 
