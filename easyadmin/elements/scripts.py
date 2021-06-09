a58d44d4fc5ed7888d06e461d10b7a3e8e71fdf7
codemation <joshjamison1@gmail.com>
2021-06-09 10:30:37 +0200

    added google signout script

--- easyadmin/elements/scripts.py
+++ easyadmin/elements/scripts.py
@@ -197,3 +197,16 @@ function loadChart{name}(){{
 
 </script>
 """
+
+def get_google_signout_script():
+    return """
+<script>
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+      window.location.href = '/admin';
+    });
+  }
+</script>
+"""
